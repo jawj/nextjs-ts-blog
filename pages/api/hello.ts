14a1fa89a176401f65eaf47cdd2e14a940a9586b
George MacKerron <george@mackerron.co.uk>
2022-09-20 13:39:34 +0100

    Using pg/neon from an API route

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Client } from 'pg';
 
 type Data = {
-  name: string
+  name: string,
+  result: any,
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const client = new Client({ connectionString: process.env.DB_URL });
+  await client.connect();
+  const { rows } = await client.query('select * from x;');
+  await client.end();
+
+  res.status(200).json({ name: 'John Doe', result: rows })
 }
