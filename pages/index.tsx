ea1386c37d692ef1af5c380ac2c7ca07867b42a7
George MacKerron <george@mackerron.co.uk>
2022-09-20 13:08:21 +0100

    First tweaks following tutorial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +17,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p>Read <Link href="/posts/first-post">this page!</Link></p>
 
         <p className={styles.description}>
           Get started by editing{' '}
